486ac02728f2ff1484eeed6a3eb25dad548e48e6
ygtorkc <109255432+ygtorkc@users.noreply.github.com>
2022-11-10 21:03:38 +0300

    Add files via upload

---
+++ src/Ustlu.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ustlu {
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int n;
+    System.out.println("sonuncu sayıyı giriniz:");
+    n = input.nextInt();
+    for(int i=1; i<=n; i *= 4){
+
+      System.out.println(i);
+
+
+
+    }
+
+
+
+
+
+
+
+
+  }
+
+
+}
